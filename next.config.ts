c861ba1fcd75873b642861a343597bd7be9fa5b7
soramameen <sorai.s.nakajima@gmai.com>
2026-05-20 20:47:31 +0900

    add: hackathon page and fix setting for image
    画像を追加したことによってpathの解決がうまくいかなかったため明示的に変更しました

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.PAGES_BASE_PATH || "/portfolio-nextjs";
+const basePath = process.env.NODE_ENV === "development" ? "" : "/portfolio-nextjs";
 
 const nextConfig: NextConfig = {
   output: "export",
   basePath,
-  assetPrefix: `${basePath}/`,
   images: { unoptimized: true },
 };
 
